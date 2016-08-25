0e0122d93d103122cf0f9def93d4b90b976738bf
Hyun-Ji Noel Jung <chunghj09@gmail.com>
2016-08-25 23:55:22 +0900

    Initial commit

---
+++ step1/teststep1.py
@@ -0,0 +1,21 @@
+print('hey Everyone')
+a = 'pig'
+c = '   '
+b = 'dad'
+print(a + '  _  ' + b)
+myTeam = ['park','young','bae','woo',"hyun","mook"]
+len(myTeam)
+print(len(myTeam))
+print(myTeam)
+print(myTeam[2])
+print(myTeam[3],myTeam[0])
+
+#튜플 자료형 - 튜플(tuple)은 몇 가지 점을 제외하곤 리스트와 거의 비슷
+   t1 = ()
+   t2 = (1,)
+   t3 = (1, 2, 3)
+   t4 = 1, 2, 3
+   t5 = ('a', 'b', ('ab', 'cd'))
+
+
+
